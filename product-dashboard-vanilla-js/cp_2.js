68bd8693a110cc580a9c8dfa414d178299f75ca0
Christopher Andrade <christopherandrade@usf.edu>
2026-06-25 13:22:35 -0400

    Update cp_2.js

--- product-dashboard-vanilla-js/cp_2.js
+++ product-dashboard-vanilla-js/cp_2.js
@@ -1,1 +1,16 @@
+// Constant URL for retrieval.
 
+const url = "https://www.course-api.com/javascript-store-products";
+
+function fetchProductsThen() {
+    fetch(url)
+        .then(response => response.json())
+        .then(products => {
+            products.forEach(product => {
+                console.log(product.fields.name);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
+}
